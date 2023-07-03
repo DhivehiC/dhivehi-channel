675f34ed10b91394fb9906f62f61eaf6ce37eb1a
Iyaaan <hassan.iyan.l@gmail.com>
2023-07-03 22:51:50 +0500

    added

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone"
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        // port: '',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3002',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dhivehichannel.mv',
+        // port: '',
+        pathname: '/**', 
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
